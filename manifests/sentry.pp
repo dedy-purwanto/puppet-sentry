55bee3fb945afb9a98dcf0f363c9f3ede39ca19a
Dedi Purwanto <kecebongsoft@gmail.com>
2012-06-24 22:56:14 +0800

    install required packages

--- manifests/sentry.pp
+++ manifests/sentry.pp
@@ -1,11 +1,31 @@
 class sentry::python(){
 
+    $packages = ["python2.7", "python-dev", "python-setuptools"]
+
+    package{$packages:
+        ensure => installed,
+    }
+
+    exec{'pip_install':
+        command => 'sudo easy_install pip',
+        require => Package[$packages]
+    }
+
+    exec{'virtualenv_install':
+        command => 'sudo easy_install virtualenv',
+        require => Package[$packages]
+    }
 }
 
 class sentry::install(){
 
+
 }
 
 class sentry(){
+    class{'sentry::python':}
+    class{'sentry::install':
+        require => Class['sentry::python']
+    }
 
 }
