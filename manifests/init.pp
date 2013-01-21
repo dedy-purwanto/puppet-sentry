3d7dc786726f93e2698df2d94f9699b1dc7776b3
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2013-01-21 18:25:14 +0100

    move entry point to init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,1 +1,9 @@
-import '*'
+class sentry($password, $salt="bf13c0"){
+    class{'sentry::python':}
+    class{'sentry::install':
+        require => Class['sentry::python'],
+        password => $password,
+        salt => $salt,
+    }
+
+}
--- manifests/sentry.pp
+++
@@ -1,9 +0,0 @@
-class sentry($password, $salt="bf13c0"){
-    class{'sentry::python':}
-    class{'sentry::install':
-        require => Class['sentry::python'],
-        password => $password,
-        salt => $salt,
-    }
-
-}
