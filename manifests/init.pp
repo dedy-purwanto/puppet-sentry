79aeff9e5030feb01913dfd8919f7d47d380c18d
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2013-01-22 12:23:26 +0100

    adapt to style guide

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,16 @@
-class sentry($password, $salt="bf13c0", $install_method=undef){
-    class{'sentry::install':
-        method => $install_method
-    }
-    class{'sentry::config':
-        password => $password,
-        salt => $salt
-    }
+class sentry (
+  $password,
+  $salt           = 'bf13c0',
+  $install_method = undef
+) {
+
+  class { 'sentry::install':
+    method => $install_method
+  }
+
+  class { 'sentry::config':
+    password => $password,
+    salt     => $salt
+  }
+
 }
