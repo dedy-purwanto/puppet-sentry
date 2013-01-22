14d87e96e4c73383b7ce9cadbdb54ddece1e9197
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2013-01-22 12:35:17 +0100

    expose more config

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,10 @@ class sentry (
   $password,
   $salt           = 'bf13c0',
   $path           = '/var/sentry',
+  $key            = '0123456789abcde',
+  $email          = 'admin@example.com',
+  $url_prefix     = undef,
+  $web_port       = 9000,
   $install_method = undef
 ) {
 
@@ -11,9 +15,13 @@ class sentry (
   }
 
   class { 'sentry::config':
-    password => $password,
-    salt     => $salt,
-    path     => $path
+    password   => $password,
+    salt       => $salt,
+    path       => $path,
+    key        => $key,
+    email      => $email,
+    url_prefix => $url_prefix,
+    web_port   => $web_port,
   }
 
 }
