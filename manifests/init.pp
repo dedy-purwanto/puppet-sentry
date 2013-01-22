ae0b2f45a7e7af794345eb1694039e98699b02aa
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2013-01-22 12:16:16 +0100

    rename method to install_method

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
-class sentry($password, $salt="bf13c0", $method=undef){
+class sentry($password, $salt="bf13c0", $install_method=undef){
     class{'sentry::install':
-        method => $method
+        method => $install_method
     }
     class{'sentry::config':
         password => $password,
