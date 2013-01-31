4429ef82a64a05b285d914fc2c91c229a18a7faf
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2013-01-31 07:41:03 -0500

    make sure to set owner and group

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,6 +4,8 @@ class sentry::config (
   $path       = '/var/sentry',
   $key        = '0123456789abcde',
   $email      = 'admin@example.com',
+  $owner      = 'sentry',
+  $group      = 'sentry',
   $url_prefix = undef,
   $web_port   = 9000
 ) {
@@ -16,17 +18,23 @@ class sentry::config (
     $hexdigest = sha1("$salt$password")
 
     file{"$path/sentry.conf.py":
-        ensure => file,
-        content => template("sentry/sentry.conf.py.erb")
+        ensure  => file,
+        content => template("sentry/sentry.conf.py.erb"),
+        owner   => $owner,
+        group   => $group,
     }
 
     file{"$path/initial_data.json":
-        ensure => file,
-        content => template("sentry/initial_data.json.erb")
+        ensure  => file,
+        content => template("sentry/initial_data.json.erb"),
+        owner   => $owner,
+        group   => $group
     }
 
     exec{"sentry_initiate":
         command => "bash -c 'source $virtualenv_path/bin/activate && sentry --config=$path/sentry.conf.py upgrade --noinput'",
+        user   => $owner,
+        group  => $group,
         require => [
             File["$path/initial_data.json"],
             Class["sentry::install"]
