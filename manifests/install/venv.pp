2d9f8edcdb2b5d72125a9a79a6318b06b47784af
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2013-05-27 12:07:03 -0400

    cleanup

--- manifests/install/venv.pp
+++ manifests/install/venv.pp
@@ -1,12 +1,15 @@
-class sentry::install::venv ($sentry_path, $virtualenv_path) {
+class sentry::install::venv (
+  $sentry_path,
+  $virtualenv_path
+) {
 
     class { 'python::venv':
       owner => 'sentry',
       group => 'sentry'
     }
 
-    python::venv::isolate { "$virtualenv_path":
-        requirements => "$sentry_path/requirements.txt",
-        requirements_source => "puppet:///modules/sentry/requirements.txt"
+    python::venv::isolate { $virtualenv_path:
+        requirements        => "${sentry_path}/requirements.txt",
+        requirements_source => 'puppet:///modules/sentry/requirements.txt'
     }
 }
