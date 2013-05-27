771cc39ccdf35e30982d20b28cf697d06a1a065d
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2013-05-27 12:08:33 -0400

    warnings cleanup

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,15 +2,19 @@ class sentry::service (
   $method = 'supervisor',
   $path   = '/var/sentry'
 ) {
-    $virtualenv_path = "$path/virtualenv"
+    $virtualenv_path = "${path}/virtualenv"
 
     case $method {
         'supervisor': {
-            class { "sentry::service::$method":
-                sentry_path => $path,
+            class { "sentry::service::${method}":
+                sentry_path     => $path,
                 virtualenv_path => $virtualenv_path
             }
         }
+        default: {
+          fail("Unknown service method '${method}' for sentry")
+        }
+
     }
 
 }
