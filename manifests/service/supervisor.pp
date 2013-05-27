dc84a36495c2d9182777aad01507660c5d3850fd
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2013-05-27 12:11:21 -0400

    more cleanup

--- manifests/service/supervisor.pp
+++ manifests/service/supervisor.pp
@@ -1,14 +1,17 @@
 # service class to be used with
 # https://github.com/plathrop/puppet-module-supervisor
-class sentry::service::supervisor ($sentry_path, $virtualenv_path) {
+class sentry::service::supervisor (
+  $sentry_path,
+  $virtualenv_path
+) {
 
   include supervisor
-  
+
   supervisor::service {
     'sentry':
       ensure      => present,
       enable      => true,
-      command     => "$virtualenv_path/bin/sentry --config=$sentry_path/sentry.conf.py start http",
+      command     => "${virtualenv_path}/bin/sentry --config=${sentry_path}/sentry.conf.py start http",
       directory   => $sentry_path,
       user        => 'sentry',
       group       => 'sentry',
