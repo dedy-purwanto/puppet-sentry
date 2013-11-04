ecc5f14cac44d76c6925e6aa091ef55ad0687fdf
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2013-11-04 18:44:52 -0500

    Use nrpe for nagios monitoring; also add selinux

--- manifests/nagios.pp
+++ manifests/nagios.pp
@@ -4,12 +4,16 @@
 #
 #
 class sentry::nagios (
-  $web_port
+  $web_port = 9100
 ) {
-    nagios::service { "sentry_${::hostname}":
-    server        => "${::hostname}",
-    check_command => 'check_http!-p 9100 localhost',
+  sentry::nrpe_service { "sentry_${web_port}":
+    plugin => 'check_http',
+    args   => "-p ${web_port} localhost"
   }
 
-  
+  # FIXME: how do we make this conditional ?
+  selinux::audit2allow { "nrpe_sentry_${web_port}":
+    content => "type=AVC msg=audit(1383607078.429:934172): avc:  denied  { name_connect } for  pid=10171 comm=\"check_http\" dest=${web_port} scontext=unconfined_u:system_r:nrpe_t:s0 tcontext=system_u:object_r:hplip_port_t:s0 tclass=tcp_socket"
+  }
+
 }
